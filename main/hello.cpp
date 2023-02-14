65e44e84c198916f279f143e0ec124b9dc125e57
Veeresh R G <veeresh.ravi@gmail.com>
2023-02-14 22:03:25 +0530

    New Change

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void)
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Hello bro I added this line" << endl;
+  cout << "Hello bro I added this line 🚀" << endl;
   return 0;
 }
